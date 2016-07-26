f92df45682a32bae2d5899c1707a6502548f1b1e
Ivan Bartsov <ivan.bartsov@gmail.com>
2016-07-27 03:00:16 +0700

    layout update, background, about section

---
+++ main.js
@@ -0,0 +1,32 @@
+// more on extensions: https://github.com/showdownjs/showdown/wiki/Cookbook:-Using-language-and-output-extensions-on-the-same-block
+showdown.extension('newsExt', function() {
+    return [
+        {
+            type: 'output',
+            filter: function(text) {
+                text.replace(/<h2/gi, '<h2 class="mdl-typography--title"');
+                return text;
+            }
+        }
+    ]
+});
+
+function loadAsMarkdown(mdLink, elementId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', mdLink, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState !== 4) return;
+        if (this.status !== 200) return;
+        // var converter = new showdown.Converter({
+        //         extensions: ['newsExt']
+        //     }),
+        // html = converter.makeHtml(this.responseText);
+        var converter = new showdown.Converter(),
+        html = converter.makeHtml(this.responseText);
+        document.getElementById(elementId).innerHTML = html;
+    };
+    xhr.send();
+}
+
+loadAsMarkdown('about.md', 'tab_content_about');
+loadAsMarkdown('news.md', 'tab_content_news');
