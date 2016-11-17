1aea05551667c807ba288bc92c0897ada751a3e1
lira54 <forward.to.ivan@gmail.com>
2016-11-17 17:27:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
     mdHelper.loadAsMarkdown('data/about.md', 'tab_content_about');
     mdHelper.loadAsMarkdown('data/news.md', 'tab_content_news');
     mdHelper.loadAsMarkdown('data/contact.md', 'tab_content_contact');
-    mdHelper.loadAsMarkdown('data/ads1.md', 'tab_content_ads1');
 });
